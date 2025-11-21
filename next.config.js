af33da6a7ebccfbd4720764801bff201c671f479
Sanduni204 <sandunihansika001@gmail.com>
2025-11-22 02:25:42 +0530

    Fix GitHub Pages export

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true }
-module.exports = nextConfig
+const nextConfig = {
+  reactStrictMode: true,
+
+  // Required for GitHub Pages
+  output: 'export',
+  basePath: '/my-portfolio',
+  assetPrefix: '/my-portfolio/',
+
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
