4b6c7ab844fca50c0a69a75e749184ab6957d226
Sanduni204 <sandunihansika001@gmail.com>
2025-11-22 14:22:20 +0530

    Change next.config.js and package.json

--- next.config.js
+++ next.config.js
@@ -1,20 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-
-  
-  output: 'export',
-
- 
-  basePath: '/my-portfolio',
-
-  
-  assetPrefix: '/my-portfolio/',
-
-  
-  images: {
-    unoptimized: true,
-  },
-};
-
-module.exports = nextConfig;
+const nextConfig = { reactStrictMode: true }
+module.exports = nextConfig
